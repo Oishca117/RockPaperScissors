0c9edfe15d2d16926090743df3409b9e23619e39
Oscar <126870071+Oishca117@users.noreply.github.com>
2023-05-24 01:35:10 -0700

    loops game but not fully functional

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function getComputerChoice() {
 }
 
 
-function playRound() {
+/* function playRound() {
     //prompt user for selection
     //save input into playerSelection
     const playerSelection = prompt("Please enter a selection: ");
@@ -26,6 +26,56 @@ function playRound() {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
     }
 
-}//end playRound
+}//end playRound */
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
 
-playRound();
+    for (playerScore = 0; playerScore < 5; ++playerScore) {
+        const playerSelection = prompt("Please enter a selection: ");
+        //computerSelection will have a function call to get computers choice of r,p,s
+        const computerSelection = getComputerChoice();
+
+        //compare playerSelection with computerSelection to determine outcome of the game
+        if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+            console.log("It's a tie!");
+        } else if (
+            (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") ||
+            (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ||
+            (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")
+        ) {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}!!`);
+        } else {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        }
+
+    }//end playRound
+
+    for (computerScore = 0; computerScore < 5; ++computerScore) {
+        const playerSelection = prompt("Please enter a selection: ");
+        //computerSelection will have a function call to get computers choice of r,p,s
+        const computerSelection = getComputerChoice();
+
+        //compare playerSelection with computerSelection to determine outcome of the game
+        if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+            console.log("It's a tie!");
+        } else if (
+            (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") ||
+            (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") ||
+            (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")
+        ) {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}!!`);
+        } else {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        }
+
+    }//end playRound
+
+    if (playerScore === 5) {
+        console.log("Player wins!");
+    } else (computerScore === 5)
+    console.log("Computer wins!");
+
+}
+
+game();
